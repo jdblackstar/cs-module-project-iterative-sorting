37963cfaf8f284aa946c8c1e0bd385fecbab4d99
jdblackstar <josh@blackstar.dev>
2020-08-18 09:55:04 -0700

    iterative_sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,47 +1,57 @@
-# TODO: Complete the selection_sort() function below
+# TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
-        smallest_index = cur_index
-        # TODO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+        smallest_index = i
 
         for k in range(cur_index, len(arr)):
             if arr[k] < arr[smallest_index]:
                 smallest_index = k
 
 
-        # TODO: swap
-        # Your code here
         arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
     return arr
 
 
-# TODO:  implement the Bubble Sort function below
+# TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
 
+    # for i in range(len(arr)):
+    if not arr:
+        return arr
+    i = 0
+    while True:
+        if (i + 1) == len(arr):
+            break
+        elif arr[i] > arr[i + 1]:
+            temp = arr[i + 1]
+            arr[i + 1] = arr[i]
+            arr[i] = temp
+
+            i = 0
+        else:
+            i += 1
 
     return arr
 
+# test = [1, 8, 2, 5, 3]
+
+# print(bubble_sort(test))
+
 '''
 STRETCH: implement the Counting Sort function below
-
 Counting sort is a sorting algorithm that works on a set of data where
 we specifically know the maximum value that can exist in that set of
 data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
-
 Each buckets[i] then is responsible for keeping track of how many times 
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
 showed up, we can construct a sorted set of the input data from the 
 buckets. 
-
 What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
